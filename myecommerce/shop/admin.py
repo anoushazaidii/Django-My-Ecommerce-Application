3d87e70ee24f607949d6899b55e21e448e7778fc
anoushazaidii <anoushazaidi432@gmail.com>
2024-06-05 17:42:06 +0500

    Checkout page ready

--- myecommerce/shop/admin.py
+++ myecommerce/shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Contact, Product
+from .models import Contact, Orders, Product
 
 admin.site.register(Product)
 admin.site.register(Contact)
+admin.site.register(Orders)
