16ff55d75f98141402610d04e469940c50a895b1
anoushazaidii <anoushazaidi432@gmail.com>
2024-06-10 14:25:46 +0500

    search functionality added

--- myecommerce/myecommerce/urls.py
+++ myecommerce/myecommerce/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
+    path('',include("shop.urls")),
     path('shop/', include("shop.urls")),
-    path('blog/', include("blog.urls")),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
